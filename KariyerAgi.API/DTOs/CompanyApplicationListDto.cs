cecdaf73e12f7768930cf46357e6d859005a12fa
Mehmet Akpınar <the.mehmetakpinar@gmail.com>
2026-03-08 21:47:42 +0300

    feat: Sirketler (IK) icin ilana gelen basvurulari listeleme (GET) API'si yazildi

---
+++ KariyerAgi.API/DTOs/CompanyApplicationListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KariyerAgi.API.DTOs
+{
+    public class CompanyApplicationListDto
+    {
+        public  int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Message { get; set; }
+        public string CvPath { get; set; }
+    }
+}
