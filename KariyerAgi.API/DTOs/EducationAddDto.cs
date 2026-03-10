70383a84c7c142a314868eb17ac4fa853faa201a
Mehmet Akpınar <the.mehmetakpinar@gmail.com>
2026-03-10 16:49:14 +0300

    feat: CV Profil modulu icin Education (Egitim) CRUD operasyonlari (POST, PUT, DELETE) yazildi

---
+++ KariyerAgi.API/DTOs/EducationAddDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KariyerAgi.API.DTOs
+{
+    public class EducationAddDto
+    {
+        public string schoolName { get; set; }
+        public string Department { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
